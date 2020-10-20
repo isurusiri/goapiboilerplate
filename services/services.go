75b4880d0cd105f1702ca2086dad8ecb3c6f683f
isurusiri <isuruanatomy@gmail.com>
2020-10-20 22:43:45 +0530

    init service implementation

--- services/services.go
+++ services/services.go
@@ -1,1 +1,17 @@
 package services
+
+// Create a new instance of the dto being passed.
+func Create() {}
+
+// GetAll returns all instances of an entity type.
+func GetAll() {}
+
+// GetOne returns an instance identified by the the id provided.
+func GetOne() {}
+
+// Update an instance with the values provided by the dto and
+// identified by the provided id.
+func Update() {}
+
+// Delete an instance identified by the provided id.
+func Delete() {}
